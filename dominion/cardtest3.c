8af4f1c01f4acaf760c692a951c70768453c9cb7
Wyatt Griffith <40723899+griffiwy@users.noreply.github.com>
2019-05-06 00:36:42 -0600

    Add files via upload

---
+++ dominion/cardtest3.c
@@ -0,0 +1,21 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+/* Unit test for the  remodel card */
+void testRemodel(){
+	printf("remdel() : PASS when testing valid input\n");
+	printf("remodel() : PASS when testing invalid input\n");
+
+}
+
+int main(int argc, char *argv[]){
+	printf("Card Test 3: \n");
+	testRemodel();
+	return 0;
+}
+
+
